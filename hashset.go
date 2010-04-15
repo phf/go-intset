21137fead97da92707ea16f2fb25fd1c3510b430
Peter Froehlich <peter.hans.froehlich@gmail.com>
2010-04-14 20:41:45 -0400

    Use max as initial size estimate for map. Actually remove elements from the map instead of setting them false. Iterate through elements.

--- hashset.go
+++ hashset.go
@@ -5,7 +5,7 @@ type Hash struct {
 }
 
 func (self *Hash) Init(max int) {
-	self.data = make(map[int]bool)
+	self.data = make(map[int]bool, max)
 }
 
 func (self *Hash) Insert(i int) {
@@ -13,9 +13,26 @@ func (self *Hash) Insert(i int) {
 }
 
 func (self *Hash) Remove(i int) {
-	self.data[i] = false
+	self.data[i] = false, false
 }
 
 func (self *Hash) Has(i int) (b bool) {
 	return self.data[i]
 }
+
+func (self *Hash) iterate(c chan<- int) {
+	for k, _ := range self.data {
+		c <- k
+	}
+}
+
+func (self *Hash) Iter() <-chan int {
+	c := make(chan int)
+	go self.iterate(c)
+	return c
+}
+
+//func (self *Hash) Union(other Set) (result Set) {
+//	res := new(Hash);
+//	return res
+//}
