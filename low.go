5037a604e328878a38edc29376a9c550e9309b03
Peter Froehlich <peter.hans.froehlich@gmail.com>
2010-04-16 00:22:58 -0400

    Funnier comment? :-D

--- low.go
+++ low.go
@@ -5,7 +5,8 @@ import (
 )
 
 // Kludge to figure out how many bits an int has. If Go ever
-// runs on machines where a byte is *not* 8 bits... Trouble!
+// ran on machines where a byte is *not* 8 bits wide... We'd
+// be screwed... :-/
 
 var bits_per_int int = unsafe.Sizeof(bits_per_int)*8
 
