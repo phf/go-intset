89e15e79f41a0011dc678e696cb0f6bf6fa9967e
Peter H. Froehlich <peter.hans.froehlich@gmail.com>
2013-10-29 20:52:26 -0400

    No more magic number.

--- low.go
+++ low.go
@@ -1,11 +1,10 @@
 package intset
 
-import (
-	"unsafe"
-)
+import "unsafe"
 
 // Kludge to figure out how many bits an int has. If Go ever
 // ran on machines where a byte is *not* 8 bits wide... We'd
 // be screwed... :-/
 
-var bitsPerInt int = int(unsafe.Sizeof(bitsPerInt) * 8)
+const bitsPerByte = 8
+var bitsPerInt int = int(unsafe.Sizeof(bitsPerInt) * bitsPerByte)
